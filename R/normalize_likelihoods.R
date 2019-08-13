7244a9d84addf7ad488933151f4b9b829fd8476f
Yunji Jong <yunji.jong@yale.edu>
2019-08-13 15:51:47 -0400

    Update documentation

--- R/normalize_likelihoods.R
+++ R/normalize_likelihoods.R
@@ -1,42 +1,55 @@
-# norm is a function that takes in a vector and returns the normalized version of the vector 
-# FIRST PART: sorts, sums up, and finds the max of the argument vector then run for loop that runs check 
-# SECOND PART: sorts, sums, and finds max of sorted and checked vector then normalizes by dividing by sum
-norm <- function(vec, decreasing = TRUE){
-	#FIRST PART 
-	sorted <- sort(vec)
-	sum <- sum(sorted)
-	max <- max(sorted)
-	count <<- NULL
-	a <<- vector()
-	
-	#for loop that goes through sorted vector and checks the value of the value minus the max to the smallest possible value
-	# if smaller, then is taken out of the vector and adds one to the count value that keeps track of removed values 
-	# if greater, add to new vector that is contains all the ones that are greater
-	for(i in sorted){
-		if (i - max > log(10 ^ (-15)) - log(length(vec))){
-			a <- c(a, i - max)
-		}
-		else{
-			count <- count + 1
-		}
-	}
-	
-	#SECOND PART
-	a_sort <- sort(a, decreasing = TRUE)
-	sum_final <- sum(a_sort)
-	final_sum <<- vector()
-	
-	#for loop that goes through a_sort and divides each value by the sum 
-	for(j in a_sort){
-		val <- j/sum_final
-		final_sum <- c(final_sum, val)
-	}
-	
-	#PRINT RESULT
-	print("FINAL RESULT")
-	for(k in final_sum){
-		print(k)
-	}
+# thresholdNorm is a function that normalizes values in a numeric vector such
+# that every value in the vector can be better compared
+
+# FIRST PART: sorts, sums up, and finds the max of the given vector the for
+# loop runs a check to compare the each value in a vector to the smallest
+# possible value in a given vector
+
+#SECOND PART: sorts, sums, and finds
+# max of resulting vector from FIRST PART normalizes the vector by dividing
+# each value in vector by sum of vector
+
+#thresholdNorm: vec, d
+#	where vec is vector containing the different likelihoods where d is the
+#	value of the exponent 
+
+thresholdNorm <- function(vec, d){
+  
+  #FIRST PART 
+  vec_sorted <- sort(vec, decreasing = TRUE)
+  vec_sum <- sum(vec_sorted)
+  vec_max <- max(vec_sorted)
+  vec_count <- NULL
+  a_i <- c(double())
+  vec_n <- length(vec)
+  
+  #for loop that goes through sorted vector and compares 
+  # the value minus the max to the smallest possible value in a given vector 
+  # if smaller, value is removed & vec_count + 1
+  # 	to keep track of removed values
+  # if greater, add value to new vector called a_i
+  for(i in vec_sorted){
+      if (i - vec_max > log(10 ^ -(d)) - log(vec_n)){
+      a_i <- c(a_i, i-vec_max)
+    }
+    else{
+      vec_count <- vec_count + 1
+    }
+  }
+  
+  #SECOND PART
+  a_sort <- sort(a_i, decreasing = TRUE)
+  sum_final <- sum(a_sort)
+  final_sum <- c(double())
+  
+  #for loop that goes through a_sort and divides each value by the sum 
+  for(j in a_sort){
+    
+    divide_sum <- j/sum_final
+    final_sum <- c(final_sum, divide_sum)
+  }
+  
+  return(final_sum)
 }
 
-norm(c(-0.001, -0.000000000001, -0.0000000000000001))
+#thresholdNorm(c(-0.001, -0.000000000001, -0.0000000000000001), 15)
