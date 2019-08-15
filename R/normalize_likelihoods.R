3101d925c298a907d469f0448dc14d02efe75449
Yunji Jong <yunji.jong@yale.edu>
2019-08-15 11:48:05 -0400

    Add comments and changes

--- R/normalize_likelihoods.R
+++ R/normalize_likelihoods.R
@@ -2,18 +2,20 @@
 requireNamespace("assertthat")
 #' @importFrom assertthat assert_that
 
+# is.wholenumber x -> T/F
+# is.wholenumber takes in a value and determines whether or not it is a 
+# whole number 
 is.wholenumber <- function(x, tol = .Machine$double.eps^0.5)  abs(x - round(x)) < tol
 
-#thresholdNorm: vec, threshold -> vec
-#	where vec is vector containing values 
-#	where d is what the base will be raised to (ex. 10^ d)
+#thresholdNorm: vec, threshold, base -> vec
+#	where vec is numeric double vector where each value is a logarithm
+#	where d is the value of the exponent (ex. 10^ d)
 
 #' @export
 ThresholdNorm <- function(input, threshold, base = 10){
   
   assert_that(length(input) > 1, msg="Input was of length 0 or 1")
   assert_that(is.numeric(input), msg="Input was not a numeric vector")
-  #assert_that(all(input < 0),    msg="Input had a nonnegative element")
   assert_that(length(base) == 1,    msg="'base' must be one number")
   assert_that(is.wholenumber(base), msg="'base must be whole number")
   assert_that(base > 1,             msg="'base' must be >1")
@@ -21,14 +23,18 @@ ThresholdNorm <- function(input, threshold, base = 10){
   assert_that(is.wholenumber(threshold), msg="'threshold' must be whole number")
   assert_that(threshold < 0,             msg="'threshold' must be <0")
 
-  vec_sorted <- sort(input, index.return = TRUE)
+  sorted <- sort(input, index.return = TRUE)
   vec_count <- NULL
   vec_n <- length(input)
   vec_max <- vec_sorted$x[vec_n]
 
-  sort_order <- vec_sorted$x
-  init_order <- vec_sorted$ix 
-  a_i <- ifelse((sort_order - vec_max) > (log(10^threshold) - log(vec_n)), 10^(sort_order - vec_max), 0)
+  vec_sorted <- vec_sorted$x
+  orig_ind <- vec_sorted$ix
+  
+  #a_i checks if 'vec_sorted' - 'vec_max' is greater than log(10 ^ 'threshold' / 'vec_n')
+  # if greater, add 10 ^ ('vec_sorted' - 'vec_max') to a_i
+  # if smaller, add 0
+  a_i <- ifelse((vec_sorted - vec_max) > (log(10^threshold) - log(vec_n)), 10^(vec_sorted - vec_max), 0)
   a_i_sum <- sum(a_i)
 
   norm_a_i <- a_i / a_i_sum
